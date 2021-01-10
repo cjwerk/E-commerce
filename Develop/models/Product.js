c75a42fe560f6e40887e4acc6cf0ceafd8756161
Chris Werkheiser <cjwerk@gmail.com>
2021-01-10 13:34:08 -0800

    category and product done

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -10,7 +10,46 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    
+      product_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
   },
+  {
+     price: {
+       type: DataTypes.DECIMAL,
+       allowNull: false,
+       valadate: {
+         isDecimal: true
+       }
+     }
+  },
+   {
+      stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaltValue: 10,
+        valadate: {
+          isNumeric: true
+        }
+      }
+   },
+   {
+      category_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'category_id',
+          key: 'id'
+        }
+      }
+   },
   {
     sequelize,
     timestamps: false,
